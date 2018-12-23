3753a96ba7beaa0eb7db8e7a73f346b27b9cc6e3
Lix <winklas.bartosz@gmail.com>
2018-12-23 21:16:51 +0100

    initial commit

---
+++ src/GitTestsOnly/Main.java
@@ -0,0 +1,13 @@
+package GitTestsOnly;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println(" This is only a test to the github!!! :) ");
+
+
+        /*
+        Coding further and further, so we need to upgrade the repository at the github :))
+         */
+    }
+}
